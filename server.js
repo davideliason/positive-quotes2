fdfb3f24e4ee44c8c79069af684fce330d29b22c
David Eliason <davethemaker@gmail.com>
2018-09-25 10:56:29 -0700

    connect to mongodb

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
 
+require('dotenv').config()
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', (req,res) => {
-	res.sendFile(__dirname + '/index.html');
-});
+MongoClient.connect(process.env.DB_HOST, (err, client) => {
+	console.log("db connected");
 
-app.post('/quotes', (req,res) => {
-	console.log(req.body);
-});
 
-app.listen(3000, () => {
-	console.log("server at 3000");
+	app.get('/', (req,res) => {
+		res.sendFile(__dirname + '/index.html');
+	});
+
+	app.post('/quotes', (req,res) => {
+		console.log(req.body);
+	});
+
+	app.listen(3000, () => {
+		console.log("server at 3000");
+	});
+
 });
