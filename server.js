ed2f45acba79d5f5a460429303b78939b13cbad8
David Eliason <davethemaker@gmail.com>
2018-09-25 11:05:05 -0700

    save inputted form data to db collection

--- server.js
+++ server.js
@@ -6,16 +6,24 @@ const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config()
 app.use(bodyParser.urlencoded({extended: true}));
 
+var db;
+
 MongoClient.connect(process.env.DB_HOST, (err, client) => {
+	if(err) return console.log(err);
 	console.log("db connected");
 
+	db = client.db('positivequotes2');
 
 	app.get('/', (req,res) => {
 		res.sendFile(__dirname + '/index.html');
 	});
 
 	app.post('/quotes', (req,res) => {
-		console.log(req.body);
+		db.collection('quotes').insertOne(req.body, (err, result) => {
+			if(err) return console.log(err);
+			console.log("saved to db :" + req.body.name + " " + req.body.quote);
+		    res.redirect('/');
+		});
 	});
 
 	app.listen(3000, () => {
