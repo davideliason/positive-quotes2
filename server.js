bae3f958440e35a42aa971f18920c943b4905c5d
David Eliason <davethemaker@gmail.com>
2018-09-25 10:53:24 -0700

    add body-parser for capturing form data

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req,res) => {
 	res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/quotes', (req,res) => {
-	res.end("got quote");
+	console.log(req.body);
 });
 
 app.listen(3000, () => {
