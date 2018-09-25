06fd0d1445892c5169f2cf1e5274d7b7c7feab20
David Eliason <davethemaker@gmail.com>
2018-09-25 10:32:49 -0700

    successfully connect to mongodb instance

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+
+require('dotenv').config();
+app.set('view engine', 'ejs')
+app.use(express.static('public'));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}));
+
+var db;
+
+MongoClient.connect(process.env.DB_HOST, (err,client) => {
+
+console.log("db connected");
 
 app.get('/', (req,res) => {
+	// console.log(process.env.TEST);
 	res.end("hello");
 });
 
 app.listen(3000);
+
+});
